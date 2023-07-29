079cc30071995244fa0f0abf4558376867fc4fc6
NGUYEN-NHUNG-113 <nhungnguyengit@gmail.com>
2023-07-29 19:09:08 +0900

    homework3_update

--- src/Employee.java
+++ src/Employee.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
 
 public class Employee  extends Person{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter ID:");
-        int id = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter name:");
-        String name = sc.nextLine();
-        System.out.println("Enter address:");
-        String address = sc.nextLine();
-        System.out.println("Enter age:");
-        int age = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter Exp :");
-        double exp = Double.parseDouble (sc.nextLine());
-        System.out.println("Enter Place Work:");
-        String placeWork = sc.nextLine();
-        Employee employee = new Employee(id, name,address,age,exp,placeWork);
-        System.out.printf(String.valueOf(employee));
-    }
-
-
-
-
 
     final double exp;
     final String placeWork;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -9,22 +6,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Mời bạn nhập tên hàng nhập kho:");
-        String nameGoods = sc.nextLine();
-        System.out.println(nameGoods);
-        System.out.println("Mời bạn nhập ngày tháng năm sinh (yyyy/MM/dd):");
-        String birthDate = sc.nextLine();
-        LocalDate lcd = LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-        System.out.println(lcd);
-
-        System.out.println("Mời bạn nhập thời gian nhập hàng (yyyy/MM/dd HH:mm:ss)");
-        String importGoods = sc.nextLine();
-        LocalDateTime lcdt = LocalDateTime.parse(importGoods,DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-        System.out.println(lcdt);
-
-        System.out.println("Mời bạn nhập thời gian (HH:mm:ss)");
-        String time = sc.nextLine();
-        LocalTime timeExport = LocalTime.parse(time,DateTimeFormatter.ofPattern("HH:mm:ss"));
-        System.out.println(timeExport);
-        }
+        System.out.println("Enter ID:");
+        int id = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter name:");
+        String name = sc.nextLine();
+        System.out.println("Enter address:");
+        String address = sc.nextLine();
+        System.out.println("Enter age:");
+        int age = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter Exp :");
+        double exp = Double.parseDouble (sc.nextLine());
+        System.out.println("Enter Place Work:");
+        String placeWork = sc.nextLine();
+        Employee employee = new Employee(id, name,address,age,exp,placeWork);
+        System.out.println(employee);
+         
+    }
+    
     }
