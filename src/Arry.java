9fcf0fa2658ab2c069e818d0919b994ac1b4af0a
Nhung-Nguyen-git <116331874+Nhung-Nguyen-git@users.noreply.github.com>
2023-07-29 19:17:34 +0900

    Update Arry.java

--- src/Arry.java
+++ src/Arry.java
@@ -1,14 +1,1 @@
-import java.util.Scanner;
 
-public class Arry {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input number");
-        int n = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < n; i++) {
-            System.out.println("moi ban nhap vao so thu" +(i + 1));
-            int x = Integer.parseInt(sc.nextLine());
-            System.out.println();
-        }
-    }
-}
